f3ec473255cf78b3de2e19faa4edabdb8e163acf
Harry-Greenaway <hmegreenaway@outlook.com>
2017-06-05 18:21:28 +0100

    1.6
    Projectiles have velocity

--- BattleTank/Source/BattleTank/Private/Tank.cpp
+++ BattleTank/Source/BattleTank/Private/Tank.cpp
@@ -19,7 +19,8 @@ void ATank::SetTurretReference(UTankTurret* TurretToSet) {
 void ATank::Fire() {
 	UE_LOG(LogTemp, Warning, TEXT("Fire request called"));
 	if (!Barrel) { return; }
-	GetWorld()->SpawnActor<AProjectile>(ProjectileBlueprint, Barrel->GetSocketLocation(FName("Projectile")), Barrel->GetSocketRotation(FName("Projectile")));
+	auto Projectile = GetWorld()->SpawnActor<AProjectile>(ProjectileBlueprint, Barrel->GetSocketLocation(FName("Projectile")), Barrel->GetSocketRotation(FName("Projectile")));
+	Projectile->LaunchProjectile(LaunchSpeed);
 }
 
 // Sets default values
